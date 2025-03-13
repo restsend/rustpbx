a90bc7aaca155e0fc62093615a9780910d365b3f
pi <shenjindi@fourz.cn>
2025-03-13 21:51:25 +0800

    update stream layout

--- src/media/stream.rs
+++ src/media/stream.rs
@@ -1,12 +1,11 @@
-use std::{collections::HashMap, sync::Mutex};
-
 use super::{
     recorder::Recorder,
     track::{Track, TrackDirection},
     vad::VADConfig,
 };
 use anyhow::Result;
-use tracing::{error, info};
+use std::sync::Mutex;
+use tokio::sync::{broadcast, mpsc};
 
 pub struct MediaStreamConfig {
     recorder: Option<String>, // Path to save the PCM recording
@@ -22,22 +21,34 @@ pub enum MediaStreamEvent {
     TranscriptionSegment(TrackDirection, u32, String), // segment-level transcription
     TrackStart(TrackDirection),
     TrackStop(TrackDirection),
+    Shutdown,
+}
+
+pub type EventSender = broadcast::Sender<MediaStreamEvent>;
+pub type EventReceiver = broadcast::Receiver<MediaStreamEvent>;
+
+pub enum MediaStreamCommand {
+    RemoveTrack(TrackDirection),
+    UpdateTrack(TrackDirection, Box<dyn Track>),
 }
 
 pub struct MediaStream {
     pub config: MediaStreamConfig,
     pub inbound: Option<Box<dyn Track>>,
     pub outbound: Mutex<Option<Box<dyn Track>>>,
-    pub recorder: Option<Recorder>,
+    pub cmd_tx: mpsc::UnboundedSender<MediaStreamCommand>,
+    pub cmd_rx: mpsc::UnboundedReceiver<MediaStreamCommand>,
 }
 
 impl MediaStream {
     pub fn new(config: MediaStreamConfig) -> Self {
+        let (cmd_tx, cmd_rx) = mpsc::unbounded_channel();
         Self {
             config,
             inbound: None,
             outbound: Mutex::new(None),
-            recorder: None,
+            cmd_tx,
+            cmd_rx,
         }
     }
 
@@ -45,11 +56,15 @@ impl MediaStream {
         todo!()
     }
 
-    pub fn remove_track(&mut self, direction: TrackDirection) {
-        todo!()
+    pub fn remove_track(&self, direction: TrackDirection) -> Result<()> {
+        self.cmd_tx
+            .send(MediaStreamCommand::RemoveTrack(direction))
+            .map_err(Into::into)
     }
 
-    pub fn update_track(&mut self, direction: TrackDirection, track: Box<dyn Track>) {
-        todo!()
+    pub fn update_track(&self, direction: TrackDirection, track: Box<dyn Track>) -> Result<()> {
+        self.cmd_tx
+            .send(MediaStreamCommand::UpdateTrack(direction, track))
+            .map_err(Into::into)
     }
 }
