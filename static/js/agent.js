13046d72c999b55ec39bf90a15302f14c2378a7e
jinti <shenjindi@fourz.cn>
2025-04-14 22:50:54 +0800

    add restsend stun server

--- static/js/agent.js
+++ static/js/agent.js
@@ -441,7 +441,7 @@ function mainApp() {
 
             const configuration = {
                 iceServers: [{
-                    urls: 'stun:stun.l.google.com:19302'
+                    urls: ['stun:stun.l.google.com:19302', 'stun:restsend.com:3478']
                 }]
             };
 
