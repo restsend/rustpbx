f35dc374cbf141acb61ac92b6412a5e6c2e93d99
jinti <shenjindi@fourz.cn>
2025-04-14 15:12:40 +0800

    demo: add stun server

--- static/js/agent.js
+++ static/js/agent.js
@@ -440,6 +440,9 @@ function mainApp() {
             }
 
             const configuration = {
+                iceServers: [{
+                    urls: 'stun:stun.l.google.com:19302'
+                }]
             };
 
             let mediaStream = await navigator.mediaDevices.getUserMedia({
