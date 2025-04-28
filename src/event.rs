3b4150a62dd153ce5045b3a09389c9688cd561fb
jinti <shenjindi@fourz.cn>
2025-04-28 15:50:44 +0800

    add asr/tts/vad endpoint params

--- src/event.rs
+++ src/event.rs
@@ -1,6 +1,7 @@
 use crate::PcmBuf;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub enum GibberishAction {
