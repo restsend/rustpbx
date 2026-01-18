0ea16fd6c79a3ada83b7469e63f5d9f694cd20e5
yeoleobun <yangli245@live.com>
2026-01-18 18:05:03 +0800

    fix transcoding timestamp

--- src/media/transcoder.rs
+++ src/media/transcoder.rs
@@ -5,6 +5,7 @@ use rustrtc::media::AudioFrame;
 pub struct Transcoder {
     decoder: Box<dyn Decoder>,
     encoder: Box<dyn Encoder>,
+    source: CodecType,
     target: CodecType,
     resampler: Option<Resampler>,
     first_input_timestamp: Option<u32>,
@@ -14,8 +15,8 @@ pub struct Transcoder {
 }
 
 impl Transcoder {
-    pub fn new(from: CodecType, target: CodecType) -> Self {
-        let decoder = create_decoder(from);
+    pub fn new(source: CodecType, target: CodecType) -> Self {
+        let decoder = create_decoder(source);
         let encoder = create_encoder(target);
 
         let source_sample_rate = decoder.sample_rate();
@@ -36,8 +37,9 @@ impl Transcoder {
         Self {
             decoder,
             encoder,
-            resampler,
+            source,
             target,
+            resampler,
             first_input_timestamp: None,
             first_input_sequence: None,
             first_output_timestamp,
@@ -46,11 +48,6 @@ impl Transcoder {
     }
 
     pub fn transcode(&mut self, frame: &AudioFrame) -> AudioFrame {
-        if self.first_input_timestamp.is_none() {
-            self.first_input_timestamp = Some(frame.rtp_timestamp);
-            self.first_input_sequence = frame.sequence_number;
-        }
-
         let mut pcmbuf = self.decoder.decode(&frame.data);
         if let Some(resampler) = &mut self.resampler {
             pcmbuf = resampler.resample(&pcmbuf);
@@ -58,25 +55,43 @@ impl Transcoder {
 
         let encoded_data = self.encoder.encode(&pcmbuf);
 
-        let first_input_ts = self.first_input_timestamp.unwrap_or(0);
-        let input_ts_delta = frame.rtp_timestamp.wrapping_sub(first_input_ts);
-        let input_clock_rate = frame.clock_rate;
-        let target_clock_rate = self.target.clock_rate();
-        let output_ts_delta =
-            (input_ts_delta as u64 * target_clock_rate as u64 / input_clock_rate as u64) as u32;
-        let output_timestamp = self.first_output_timestamp.wrapping_add(output_ts_delta);
-
-        let first_input_seq = self.first_input_sequence.unwrap_or(0);
-        let input_seq = frame.sequence_number.unwrap_or(0);
-        let seq_delta = input_seq.wrapping_sub(first_input_seq);
-        let output_sequence = self.first_output_sequence.wrapping_add(seq_delta);
+        let (rtp_timestamp, output_sequence) = self.new_timestamp(frame);
 
         AudioFrame {
-            rtp_timestamp: output_timestamp,
+            rtp_timestamp,
             clock_rate: self.target.clock_rate(),
             data: encoded_data.into(),
             sequence_number: Some(output_sequence),
             payload_type: Some(self.target.payload_type()),
         }
+    }
+
+    pub fn update_dtmf_timestamp(&mut self, frame: &mut AudioFrame) {
+        let (rtp_timestamp, output_sequence) = self.new_timestamp(frame);
+        frame.rtp_timestamp = rtp_timestamp;
+        frame.sequence_number = Some(output_sequence);
+    }
+
+    fn new_timestamp(&mut self, frame: &AudioFrame) -> (u32, u16) {
+        if self.first_input_timestamp.is_none() {
+            self.first_input_timestamp = Some(frame.rtp_timestamp);
+            self.first_input_sequence = frame.sequence_number;
+        }
+
+        let source_clock_rate = self.source.clock_rate();
+        let target_clock_rate = self.target.clock_rate();
+
+        let first_input_ts = self.first_input_timestamp.unwrap_or_default();
+        let input_ts_delta = frame.rtp_timestamp.wrapping_sub(first_input_ts);
+        let output_ts_delta =
+            (input_ts_delta as u64 * target_clock_rate as u64 / source_clock_rate as u64) as u32;
+        let output_timestamp = self.first_output_timestamp.wrapping_add(output_ts_delta);
+
+        let first_input_seq = self.first_input_sequence.unwrap_or_default();
+        let input_seq = frame.sequence_number.unwrap_or_default();
+        let seq_delta = input_seq.wrapping_sub(first_input_seq);
+        let output_sequence = self.first_output_sequence.wrapping_add(seq_delta);
+
+        (output_timestamp, output_sequence)
     }
 }
