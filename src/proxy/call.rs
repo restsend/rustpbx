0d2c049cbec17057417fd7f35b0fc26d26a9d538
yeoleobun <yangli245@live.com>
2026-04-12 16:36:08 +0800

    fix: update requre-URI with registered address

--- src/proxy/call.rs
+++ src/proxy/call.rs
@@ -460,20 +460,21 @@ impl CallModule {
             }
         };
 
-        let mut loc = Location {
+        let loc = Location {
             aor: callee_uri.clone(),
             ..Default::default()
         };
+        let mut locs = vec![loc];
         let mut internal_lookup_empty = false;
 
         if callee_is_same_realm {
             if let Ok(results) = self.inner.server.locator.lookup(&callee_uri).await {
                 internal_lookup_empty = results.is_empty();
-                loc.supports_webrtc |= results.iter().any(|item| item.supports_webrtc);
+                if !results.is_empty() {
+                    locs = results;
+                }
             }
         }
-
-        let locs = vec![loc];
         let caller_uri = match caller.from.as_ref() {
             Some(uri) => uri.clone(),
             None => original
@@ -482,7 +483,6 @@ impl CallModule {
                 .uri()
                 .map_err(|e| RouteError::from((anyhow::anyhow!(e), None)))?,
         };
-
         let preview_option = InviteOption {
             callee: callee_uri.clone(),
             caller: caller_uri.clone(),
